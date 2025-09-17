064078331780ce37c168848857035af34d731bf9
levin jones <levinjones@gmail.com>
2025-09-17 09:02:06 +0300

    Implemented CRUD http methods for the product entity. Modified mappers and product controller packages

--- src/main/java/com/lesjones/store/mappers/ProductMapper.java
+++ src/main/java/com/lesjones/store/mappers/ProductMapper.java
@@ -4,10 +4,16 @@ import com.lesjones.store.dtos.ProductDto;
 import com.lesjones.store.entities.Product;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
     @Mapping(source = "category.id", target = "categoryId")
     ProductDto toDto(Product product);
+
+    Product toEntity(ProductDto productDto);
+
+    @Mapping(target = "id", ignore = true)
+    void update(ProductDto productDto, @MappingTarget Product product);
 }

0f641ecce3bcd73c6a62d1665f2992460d0163f1
levin jones <levinjones@gmail.com>
2025-09-18 07:42:39 +0300

    Added validation logic to the user class. Introduced unique email for registering users.

--- src/main/java/com/lesjones/store/controllers/UserController.java
+++ src/main/java/com/lesjones/store/controllers/UserController.java
@@ -4,17 +4,17 @@ import com.lesjones.store.dtos.ChangePasswordRequest;
 import com.lesjones.store.dtos.RegisterUserRequest;
 import com.lesjones.store.dtos.UpdateUserRequest;
 import com.lesjones.store.dtos.UserDto;
-import com.lesjones.store.entities.User;
 import com.lesjones.store.mappers.UserMapper;
 import com.lesjones.store.repositories.UserRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Map;
 import java.util.Set;
 
 
@@ -51,9 +51,15 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDto> createUser(
-            @RequestBody RegisterUserRequest request,
+    public ResponseEntity<?> registerUser(
+            @Valid @RequestBody RegisterUserRequest request,
             UriComponentsBuilder uriBuilder) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            return ResponseEntity.badRequest().body(
+                    Map.of("email", "Email is already registered")
+            );
+        }
+
         var user = userMapper.toEntity(request);
         userRepository.save(user);
 

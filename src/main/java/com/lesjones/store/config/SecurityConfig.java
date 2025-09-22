7d2ddc64cf9da622dc6a9e4aca7cd7b3434d0b09
levin jones <levinjones@gmail.com>
2025-09-22 23:53:09 +0300

    Implemented jwt validation.

--- src/main/java/com/lesjones/store/config/SecurityConfig.java
+++ src/main/java/com/lesjones/store/config/SecurityConfig.java
@@ -53,6 +53,7 @@ public class SecurityConfig {
                     .requestMatchers("/carts/**").permitAll()
                     .requestMatchers(HttpMethod.POST,"/users").permitAll()
                     .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/auth/validate").permitAll()
                     .anyRequest().authenticated()
             );
 

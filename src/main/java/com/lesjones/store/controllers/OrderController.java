e76a8e21f843098ad1581c0a0f131bacf0272919
levin jones <levinjones@gmail.com>
2025-10-11 17:21:16 +0300

    Implement functionality to get a single order

--- src/main/java/com/lesjones/store/controllers/OrderController.java
+++ src/main/java/com/lesjones/store/controllers/OrderController.java
@@ -1,12 +1,15 @@
 package com.lesjones.store.controllers;
 
+import com.lesjones.store.dtos.ErrorDto;
 import com.lesjones.store.dtos.OrderDto;
+import com.lesjones.store.exceptions.OrderNotFoundException;
 import com.lesjones.store.services.OrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 @AllArgsConstructor
@@ -19,5 +22,20 @@ public class OrderController {
     @GetMapping
     public List<OrderDto> getAllOrders() {
         return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{orderId}")
+    public OrderDto getOrder(@PathVariable("orderId") Long orderId) {
+        return orderService.getOrder(orderId);
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<Void> handleOrderNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorDto> handleAccessDeniedException(Exception e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorDto(e.getMessage()));
     }
 }
--- src/main/java/com/lesjones/store/services/OrderService.java
+++ src/main/java/com/lesjones/store/services/OrderService.java
@@ -1,9 +1,11 @@
 package com.lesjones.store.services;
 
 import com.lesjones.store.dtos.OrderDto;
+import com.lesjones.store.exceptions.OrderNotFoundException;
 import com.lesjones.store.mappers.OrderMapper;
 import com.lesjones.store.repositories.OrderRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,7 +20,20 @@ public class OrderService {
 
     public List<OrderDto> getAllOrders() {
         var user = authService.getCurrentUser();
-        var orders = orderRepository.getAllByCustomer(user);
+        var orders = orderRepository.getOrdersByCustomer(user);
         return orders.stream().map(orderMapper::toDto).toList();
+    }
+
+    public OrderDto getOrder(Long orderId) {
+        var order = orderRepository
+                .getOrderWithItems(orderId)
+                .orElseThrow(OrderNotFoundException::new);
+
+        var user = authService.getCurrentUser();
+        if (!order.isPlacedBy(user)) {
+            throw new AccessDeniedException("You don't have access to this order.");
+        }
+
+        return orderMapper.toDto(order);
     }
 }
